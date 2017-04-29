e270036d854f9e55855652b1e4ba1d47b6652e00
questionyu <questionyu@outlook.com>
2017-04-29 21:19:56 +0800

    Add CreditHistoryException class.

---
+++ src/CreditHistoryException.java
@@ -0,0 +1,12 @@
+/**
+ * Title        CreditHistoryException.java
+ * Description
+ */
+class CreditHistoryException extends RuntimeException {
+	CreditHistoryException() {
+	}
+
+	CreditHistoryException(String message) {
+		super(message);
+	}
+}
