0eac21289d73e873f7c9bde292eff65e51f33ddf
questionyu <questionyu@outlook.com>
2017-04-30 18:07:13 +0800

    Optimize JuniorAccount class.

--- src/JuniorAccount.java
+++ src/JuniorAccount.java
@@ -5,10 +5,9 @@
 class JuniorAccount extends BankAccount {
 	private int age;
 
-	JuniorAccount(double initBalance, String name, String address, int birth, int age) {
+	JuniorAccount(double initBalance, String name, String address, int birth, int age) throws IllegalAgeException {
 		super(initBalance, name, address, birth);
 		if (age > 16)
-			//System.out.println("Age > 16 can not open junior account.");
 			throw new IllegalAgeException();
 		this.age = age;
 	}
