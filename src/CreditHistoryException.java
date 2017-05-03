a161e4bbf0c7230665608bf825f77426ec5baa1b
questionyu <questionyu@outlook.com>
2017-05-03 23:38:55 +0800

    Remove two exception classes's useless constructor and add the description of two classes.

--- src/CreditHistoryException.java
+++ src/CreditHistoryException.java
@@ -1,12 +1,8 @@
 /**
  * Title        CreditHistoryException.java
- * Description
+ * Description  This class's instance should be thrown when the customer's credit history is bad.
  */
 class CreditHistoryException extends RuntimeException {
 	CreditHistoryException() {
-	}
-
-	CreditHistoryException(String message) {
-		super(message);
 	}
 }
--- src/IllegalAgeException.java
+++ src/IllegalAgeException.java
@@ -1,12 +1,8 @@
 /**
  * Title        IllegalAgeException.java
- * Description
+ * Description  This class's instance should be thrown when the customer's age is not suit for account's limitation.
  */
 class IllegalAgeException extends RuntimeException {
 	IllegalAgeException() {
-	}
-
-	IllegalAgeException(String message) {
-		super(message);
 	}
 }
